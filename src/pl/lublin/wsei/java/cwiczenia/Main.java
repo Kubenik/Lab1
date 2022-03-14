b264b5e71e8ff1374c627412324c5b5300592af8
Kubenik <kubenik22@gmail.com>
2022-03-15 00:11:05 +0100

    Zadania od 1-6

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,16 @@
+package pl.lublin.wsei.java.cwiczenia;
+/*
+*
+ @param kom;
+ * */
+public class Main {
+
+    public static void main(String[] args) {
+	//kom
+        /*kom*/
+        System.out.println("Hello java World\n");
+
+
+
+    }
+}
