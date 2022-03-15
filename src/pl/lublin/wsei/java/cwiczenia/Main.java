a3d0816e58c818860814b1b0cc010720c8984d88
Kubenik <kubenik22@gmail.com>
2022-03-15 19:28:42 +0100

    Zadanie 9 pętla z break

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -8,14 +8,13 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int num1 = 0, num2 = 0;
         do {
-            System.out.print("Podaj pierwszą liczbę: ");
+            System.out.print("Podaj liczby które mam dodać: ");
             num1 = input.nextInt();
-            if (num1 != 0) {
-                System.out.print("Podaj drugą liczbę: ");
-                num2 = input.nextInt();
-            }
+            num2 = input.nextInt();
+                if((num1 == 0) || (num2 == 0))break;
+
             System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1 + num2);
-        } while ((num1 != 0) && (num2 != 0));
+        } while (true);
     }
 
 }
