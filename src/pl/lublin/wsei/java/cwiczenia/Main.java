e1bf0a082d58f4b56e140951188d6b798ecea5e1
Kubenik <kubenik22@gmail.com>
2022-03-15 22:03:12 +0100

    Zadanie 12 c) uruchomiony program.

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,31 +1,16 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("DEC: ");
-        Scanner input = new Scanner(System.in);
-        int DEC = 0;
-        DEC = input.nextInt();
-        String BIN, HEX;
-        HEX = Integer.toHexString(DEC);
-        BIN = Integer.toBinaryString(DEC);
-        String BIN2,HEX2;
-        BIN2 = leftPad(BIN, '0', 8);
-        HEX2 = leftPad(HEX, '0', 4);
-        System.out.printf("BIN = " + BIN2 + "\nHEX = " + HEX2.toUpperCase());
-
-    }
-    public static String leftPad(String aText, char aChar, int aWidth){
-        String res = aText;
-
-        for (int i = 0; i < aWidth - aText.length();i++)
-            res = aChar + res;
-        return res;
-
-
+        Account acc = new Account();
+        acc.setName("Jakub Zyga");
+        System.out.println(acc.getName());
     }
 
 }
